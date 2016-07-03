472735083137f31d124d96f8f8d8627cd7298898
stephens999 <stephens999@gmail.com>
2016-07-03 10:40:31 -0500

    update docs

--- R/trunct.R
+++ R/trunct.R
@@ -18,30 +18,33 @@ e_trunct_onesided = function(q,v,r){
 }
 
 #' Compute moments of univariate truncated t distribution
-#' @details This function computes the $r$th moment of the univariate t distribution on v degrees of freedom,
+#' @details This function computes the r-th moment of the univariate t distribution on df degrees of freedom,
 #' truncated to the interval (a,b). If parameters are vectors then the r[i]th moment is computed for each (a[i],b[i],v[i])
-#' The methods are based on results in O'Hagan, Biometrika 1973 and work for v>r. Otherwise NaN is returned.
+#' The methods are based on results in O'Hagan (1973) and work for v>r. Otherwise NaN is returned.
 #' @param a the left end(s) of the truncation interval(s)
 #' @param b the right end(s) of the truncation interval(s)
-#' @param v the degrees of freedom of the t distribution
+#' @param df the degrees of freedom of the t distribution
 #' @param r the degree of moment to compute
 #'
-#' @examples
-#'  e_trunct(-3,3,3,2) # second moment of t distribution on 2df truncated to (-3,3)
-#'  e_trunct(-3,3,3,1) # first moment of same, should be 0
+#' @references O'Hagan, A. (1973) Bayes estimation of a convex quadratic. \emph{Biometrika}  \strong{60} (3).
 #'
-#'  e_trunct(c(-3,0),c(0,0),4,2) # the function is vectorized
+#' @examples
+#'  e_trunct(-3,3,3,2) # second moment of t distribution on 3df truncated to (-3,3)
+#'  e_trunct(-2,2,4,1) # first moment, should be 0 by symmetry
+#'
+#'  e_trunct(c(-3,-2),c(3,2),c(3,4),c(2,1)) # the function is vectorized
+#'
 #'
 #' @export
-e_trunct = function(a,b,v,r){
+e_trunct = function(a,b,df,r){
   mult=ifelse(a>0 & b>0,-1,1) # calculations more stable for negative
   #a and b, but in this case need to multiply result by (-1)^r
   aa = ifelse(a>0 & b>0, -b, a)
   bb = ifelse(a>0 & b>0, -a, b)
 
   mult^r * ifelse(aa==bb,aa^r,
-         (stats::pt(aa,v,lower.tail=FALSE)*e_trunct_onesided(aa,v,r) -
-              stats::pt(bb,v,lower.tail=FALSE)*e_trunct_onesided(bb,v,r))/(stats::pt(bb,v)-stats::pt(aa,v)))
+         (stats::pt(aa,df,lower.tail=FALSE)*e_trunct_onesided(aa,df,r) -
+              stats::pt(bb,df,lower.tail=FALSE)*e_trunct_onesided(bb,df,r))/(stats::pt(bb,df)-stats::pt(aa,df)))
 }
 
 
